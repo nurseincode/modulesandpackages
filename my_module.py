e324d7616180b6c28b314499ad7f708a14d20926
https://github.com/nurseincode <nurseincode@gmail.com>
2025-10-01 20:10:44 +1000

    imported module

---
+++ my_module.py
@@ -0,0 +1,11 @@
+greeting = 'Hello World'
+
+nums = [10, 20 , 30]
+
+person = {
+    'name': 'John',
+    'age': 20
+}
+
+def foo(person):
+    print(f'{person.get('name')} is {person.get('age')} years old.')
