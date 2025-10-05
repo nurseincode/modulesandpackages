a3d2f7fb0ee65844bc73f6f53e5e7b5e5b778cb7
https://github.com/nurseincode <nurseincode@gmail.com>
2025-10-05 18:14:55 +1100

    modules and packaages completed

--- main.py
+++ main.py
@@ -7,5 +7,11 @@
 # from my_module import person, foo # selective import
 #foo(person)
 
-import modules.my_module as my_module 
-print(dir(my_module))
+# import modules.my_module as my_module # as keyword creates an alias for my-module
+# print(dir(my_module))
+
+from color50 import rgb, constants
+
+my_color = rgb(128, 0, 128)
+# print(my_color + "Hello, World!" + constants.RESET) # Sets terminal color to purple
+print(constants.YELLOW + "Hello World!" + constants.RESET) # to yellow
